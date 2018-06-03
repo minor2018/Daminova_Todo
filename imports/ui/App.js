d333af63ed9f0f81e656462d3b402544a0e454c5
Daminova Endzhe <endzhdamin@gmail.com>
2018-06-03 22:45:17 +0300

    To Do List Daminova

---
+++ imports/ui/App.js
@@ -0,0 +1,83 @@
+﻿import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Tasks } from '../api/tasks.js';
+import Task from './Task.js';
+import { Panel, Button } from 'react-bootstrap';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+
+handleSubmit(event) {
+    event.preventDefault();
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
+   renderTasks() {
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
+       <Task key={task._id} task={task} />
+     ));
+  }
+  render() {
+     return (
+       <div className="container">
+         <header>
+           <h1>To Do List</h1>
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Скрыть выполненные задачи
+          </label>
+ <h2>Невыполненных задач: {this.props.incompleteCount}</h2>
+
+            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Введите новую задачу и нажмите Enter"
+            />
+          </form>
+         </header>
+ 
+         <ul>
+           {this.renderTasks()}
+         </ul>
+       </div>
+     );
+   }
+ }
+
+
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
+  };
+})(App);
+
---
+++ imports/ui/Task.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import { Tasks } from '../api/tasks.js';
+import { Button, Glyphicon, ButtonGroup } from 'react-bootstrap';
+
+export default class Task extends Component {
+toggleChecked() {
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked },
+    });
+  }
+ 
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }  
+render() {
+    const taskClassName = this.props.task.checked ? 'checked' : '';
+    return (
+      <li className={taskClassName}>
+                      <ButtonGroup>
+
+                      <Button bsStyle="danger" onClick={this.deleteThisTask.bind(this)}>
+                          <Glyphicon glyph="remove"/>
+                      </Button>
+
+
+                              <Button bsStyle={!!this.props.task.checked?"success":"warning"} onClick={this.toggleChecked.bind(this)}>
+                                  <Glyphicon glyph={!!this.props.task.checked?"ok":"repeat"}/>
+                              </Button>
+                      </ButtonGroup>
+
+
+
+                      <span className="text">{this.props.task.text}</span>
+
+                  </li>
+              );
+          }
+      }
